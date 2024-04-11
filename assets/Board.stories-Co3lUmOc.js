117ea26929e9a124a24104dd7b4cca7c8339679e
vitaliyheliotrope <vitaliy@heliotrope.digital>
2024-04-11 17:35:48 +0300

    build

---
+++ assets/Board.stories-Co3lUmOc.js
@@ -0,0 +1,10 @@
+import{j as e}from"./styled-components.browser.esm-D848Jn1s.js";import{B as r}from"./Board-D97at8dN.js";import{B as m}from"./Box-I8wZGDRa.js";import"./index-Cu9bd8lq.js";import"./iframe-BnNJRw71.js";import"../sb-preview/runtime.js";import"./util-5knA4vmh.js";import"./extends-CCbyfPlC.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-Bcg3o4Lk.js";const T={title:"Components/Board",component:r,tags:["autodocs"],render:s=>e.jsx(m,{sx:{maxWidth:400,margin:"auto",marginTop:"10px"},children:e.jsx(r,{...s})})},o={args:{colorTheme:"default",pieceType:"alpha",coordinates:"in",threat:!1,threatFrom:"e2",threatTo:"e4"}};var t,a,n;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    colorTheme: 'default',
+    pieceType: 'alpha',
+    coordinates: 'in',
+    threat: false,
+    threatFrom: 'e2',
+    threatTo: 'e4'
+  }
+}`,...(n=(a=o.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const g=["Component"];export{o as Component,g as __namedExportsOrder,T as default};
